b0328639fcfe2047c68d39831ae13df4a8910bc0
Hiren Parmar <hiren.parmar@workday.com>
2020-08-21 22:38:48 -0700

    Added Login/Logout functionality, Confirm Delete Menu Item, Admin Operations as Admin Home, refactoring of Admin Operations

--- src/components/admin/AdminHome.js
+++ src/components/admin/AdminHome.js
@@ -5,17 +5,21 @@ import AllMenuItemsView from "../../common/views/AllMenuItemsView";
 import AddMenuView from "../../common/views/AddMenuView";
 import OrdersListView from "../../common/views/OrdersListView";
 import UpdateMenuView from "../../common/views/UpdateMenuView";
+import AdminView from "../../common/views/AdminView";
+import {AuthProvider} from "../../common/Auth";
+import PrivateRoute from "../../common/PrivateRoute";
 
 function AdminHome({match}) {
     return <>
-        <Switch>
-            <Route exact path={`${match.path}/orders`} component={OrdersListView}/>
-            <Route exact path={`${match.path}/add-new-menu`} component={AddMenuView}/>
-            <Route exact path={`${match.path}/all-menu-items`} component={AllMenuItemsView}/>
-            <Route exact path={`${match.path}/update-menu-item`} component={UpdateMenuView}/>
-            {/*<Route exact path={`${match.path}/operations`} component={AdminOperations}/>*/}
-            {/*<Redirect from={match.path} to={`${match.path}/operations`}/>*/}
-        </Switch>
+        <AuthProvider>
+            <Switch>
+                <PrivateRoute exact path={`${match.path}/orders`} component={OrdersListView}/>
+                <PrivateRoute exact path={`${match.path}/add-new-menu`} component={AddMenuView}/>
+                <PrivateRoute exact path={`${match.path}/all-menu-items`} component={AllMenuItemsView}/>
+                <PrivateRoute exact path={`${match.path}/update-menu-item`} component={UpdateMenuView}/>
+                <Route exact path={`${match.path}/`} component={AdminView}/>
+            </Switch>
+        </AuthProvider>
     </>;
 }
 
