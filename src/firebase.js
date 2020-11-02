f4f720c87d3e07113ace126910d1f6e2384b8342
Hiren Parmar <hirenparmar_r@yahoo.com>
2020-11-01 21:46:09 -0800

    Auto Firebase Config

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDudZwUmnLW9bhjVNKvAaSFKOIq-qtOt8E",
-    authDomain: "snack-shack-test.firebaseapp.com",
-    databaseURL: "https://snack-shack-test.firebaseio.com",
-    projectId: "snack-shack-test",
-    storageBucket: "snack-shack-test.appspot.com",
-    messagingSenderId: "645218517573",
-    appId: "1:645218517573:web:45ac65a77e13c0ca38592c"
+    apiKey: "<API-KEY>",
+    authDomain: "<PROJECT-ID>.firebaseapp.com",
+    databaseURL: "https://<PROJECT-ID>.firebaseio.com",
+    projectId: "<PROJECT-ID>",
+    storageBucket: "<PROJECT-ID>.appspot.com",
+    messagingSenderId: "<SENDER_ID>",
+    appId: "<APP_ID>"
 };
 
 firebase.initializeApp(firebaseConfig);
