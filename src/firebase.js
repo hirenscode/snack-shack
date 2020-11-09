b70feac4710489f90c01bca60c03b59fe5072c16
Hiren Parmar <hirenparmar_r@yahoo.com>
2020-11-08 19:58:35 -0800

    Added functions to Firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/storage';
+import 'firebase/functions';
 
 let firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
